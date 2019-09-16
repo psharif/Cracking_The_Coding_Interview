14c4b9e0b13796f81dc8ccc6ae83242bb60a28d8
psharif <psharif2788@gmail.com>
2019-09-15 17:40:30 -0700

    Added Animal Shelter file

--- Stacks And Queues/animalShelter.js
+++ Stacks And Queues/animalShelter.js
@@ -1,3 +1,56 @@
-class AnimalShelter {
-  
+const { LinkedList, Node } = require('../Linked\ Lists/LinkedListES6.js');
+
+class animalList extends LinkedList {
+  enqueue(node) {
+    let head = this.head;
+    if(head == null){
+      head.next = node;
+    }
+    else {
+
+    }
+
+  }
 }
+
+class Cat {
+  constructor(val) {
+    this.catName = val;
+    this.recieved = new Date();
+  }
+
+  getCat(){
+    return new Node(this);
+  }
+}
+
+class Dog {
+  constructor() {
+    this.catName = val;
+    this.recieved = new Date();
+  }
+
+  getDog() {
+    return new Node(this);
+  }
+}
+
+
+class AnimalShelter {
+  constructor() {
+    this.catList = new LinkedList();
+    this.dogList = new dogList();
+  }
+
+  addAnimal(animal) {
+    if(animal instanceOf Cat){
+      catList.
+    }
+  }
+
+
+}
+
+
+
+const linked = new LinkedList();
