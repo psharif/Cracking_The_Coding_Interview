14865cc5141473ddcd9b12b3fd65afc413509005
psharif <psharif2788@gmail.com>
2019-10-06 16:13:42 -0700

    Made some changes to checkBalanced algorithm

--- Trees and Graphs/checkBalanced.js
+++ Trees and Graphs/checkBalanced.js
@@ -5,31 +5,67 @@ const { TreeNode, BinarySearchTree } = require("./binarySearchTree.js");
 // For the purposes of this question, a balanced tree is defined to be a tree
 // such that the heights of the two subtrees of any node never differ by more than one.
 
-const getHeight = root => {
+/// Simpler Solution
+const getHeight1 = root => {
   if (root === null) return -1;
-  else return Math.max(getHeight(root.left), getHeight(root.left)) + 1;
+  else return Math.max(getHeight1(root.left), getHeight1(root.left)) + 1;
 };
 
-const checkBalanced = root => {
+const checkBalanced1 = root => {
   if (root == null) return true;
 
   const heightDiff = getHeight(root.left) - getHeight(root.right);
   /// Make sure both branches are balanced.
   if (Math.abs(heightDiff) > 1) return false;
-  else return checkBalanced(root.left) && checkBalanced(root.right);
+  else return checkBalanced1(root.left) && checkBalanced1(root.right);
+};
+
+/// Optimal Solution
+const getHeight2 = root => {
+  if (root == null) return -1;
+
+  const leftHeight = getHeight2(root.left);
+  if (leftHeight == "error") return "error";
+
+  const rightHeight = getHeight2(root.right);
+  if (leftHeight == "error") return "error";
+
+  const heightDiff = getHeight2(root.left) - getHeight2(root.right);
+  /// Make sure both branches are balanced.
+  if (Math.abs(heightDiff) > 1) return "error";
+  else return Math.max(leftHeight, rightHeight) + 1;
+};
+
+const checkBalanced2 = root => {
+  return getHeight2(root) !== "error";
 };
 
 ///Create a Tree
 const tree = new BinarySearchTree();
+const errorTree = new BinarySearchTree();
 
-tree.insert(7);
-tree.insert(5);
-tree.insert(6);
+/*
+/// Error tree
 tree.insert(4);
-tree.insert(1);
+tree.insert(2);
+tree.insert(6);
+tree.insert(5);
+tree.insert(7);
 tree.insert(3);
-tree.insert(10);
+tree.insert(1);
 
-//tree.inorder(tree.getRootNode());
+/// Error tree
+errorTree.insert(7);
+errorTree.insert(5);
+errorTree.insert(6);
+errorTree.insert(4);
+errorTree.insert(1);
+errorTree.insert(3);
+errorTree.insert(10);
 
-console.log(checkBalanced(tree.getRootNode()));
+console.log(checkBalanced2(tree.getRootNode()));
+console.log(tree);
+console.log();
+console.log(checkBalanced2(errorTree.getRootNode()));
+console.log(errorTree);
+*/
